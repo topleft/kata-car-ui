dea864487d8382dc1f852d167ef294881d5d1302
Pete Jeffryes <pjeffryes@tendrilinc.com>
2018-10-02 20:06:38 -0600

    working, about to refactor

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,6 +4,14 @@ import './styles.scss';
 
 
 class Header extends React.Component {
+  listItems = [
+    'Search Cars',
+    'Buyers Tips',
+    'Finance',
+    'Sign In',
+
+  ]
+
   constructor() {
     super();
   }
@@ -16,10 +24,8 @@ class Header extends React.Component {
             <input id='hamburger' type="checkbox"/>
             <div className='header__navigation__menu'>
               <ul className='header__navigation__menu__list'>
-                <li onClick={() => console.log('wow')}>wow</li>
-                <li>wow</li>
-                <li>wow</li>
-                <li>wow</li>
+                {this.listItems.map((item, i) => <li key={i}>{item}</li>)}
+
               </ul>
             </div>
           </label>
